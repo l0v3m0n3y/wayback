1d5a863e914b22597e4d718808d5bfb31671c48e
in my mind... <94906343+l0v3m0n3y@users.noreply.github.com>
2025-10-29 01:16:05 +0300

    refactor: remove wayback.py from root (moved to src/)

--- wayback.py
+++
@@ -1,8 +0,0 @@
-import requests
-class Client():
-	def __init__(self):
-		self.api="http://archive.org/wayback"
-	def url_timestamp(self,url:str,time:str):
-		return requests.get(f"{self.api}/available?url={url}&timestamp={time}").json()
-	def available_url(self,url:str):
-		return requests.get(f"{self.api}/available?url={url}").json()
